a91d8c9ff34626585655e6241e77ac9480a230c5
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-01 14:45:45 +0430

    13 use yargs instead of process.argv lesson

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-// const fs = require('fs')
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { alias } = require('yargs');
 
-// const persons = [
-//     { id: 1, name: 'John', age: 14 },
-//     { id: 2, name: 'Jane', age: 22 },
-//     { id: 3, name: 'Doe', age: 30 },
-// ]
-
-// // fs.writeFileSync('persons.json', JSON.stringify(persons))
-
-// const data = fs.readFileSync('persons.json')
-// const personsFromFile = JSON.parse(data.toString())
-// console.log('personsFromFile = ', personsFromFile)
-
-
-console.log(process.argv)
-
-const command = process.argv[2]
-
-// if (command === 'add') {
-//     console.log('Adding a new person...')
-// } else if (command === 'remove') {
-//     console.log('Removing a person...')
-// } else {
-//     console.log('Unknown command. Please use "add" or "remove".')
-// }
-
-switch (command) {
-    case 'add':
-        console.log('Adding a new person...')
-        break
-    case 'remove':
-        console.log('Removing a person...')
-        break
-    default:
-        console.log('Unknown command. Please use "add" or "remove".')
-        break
-}
-
+yargs(hideBin(process.argv))
+    .command({
+        command: 'create',
+        aliases: ['c'],
+        describe: '[create new contact]',
+        builder: {
+            fullname: {
+                alias: 'f',
+                describe: 'Full name of the contact',
+                demandOption: true,
+                type: 'string',
+            },
+            phone: {
+                alias: 'p',
+                describe: 'Phone number of the contact',
+                demandOption: true,
+                type: 'number',
+            },
+            email: {
+                alias: 'e',
+                describe: 'Email address of the contact',
+                demandOption: false,
+                type: 'string',
+            }
+        },
+        handler: function (argv) {
+            console.log("Creating contact with name:", argv.fullname);
+            console.log("Creating contact with phone:", argv.phone);
+            console.log("Creating contact with email:", argv.email);
+        },
+    })
+    .parse(); // <-- بدون این خط دستورات اجرا نمی‌شوند
