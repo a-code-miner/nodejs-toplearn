7ac94d80fee196e53c094784ebea9371ec67ee4d
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-12 10:37:21 +0430

    27 helper functions and because of old method it did not work

--- routes/home.js
+++ routes/home.js
@@ -1,27 +1,18 @@
 
+import express, { Router } from 'express'
 
-
-import express from 'express'
-import { Router } from 'express'
+import { getRootPath } from '../utils/path.js'
 
 const router = Router()
 
 router.get('/', (req, res, next) => {
-    // res.send('<form action="/admin/message" method="POST"><input type="text" name="message" /><input type="submit" /></form>')
     res.sendFile('index.html', { root: 'views' }) // new version
+    // res.sendFile(path.join(getRootPath(), 'views', 'index.html')) // old version // did not work
 })
 
 router.get('/hello', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.sendFile('hello.html', { root: 'views' }) // new version
 })
 
 export default router
-
-
-// This syntax is used for ES6 modules and it's not compatible with CommonJS.
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// res.sendFile(path.join(__dirname, '..', 'views', 'index.html')) // old version
 
