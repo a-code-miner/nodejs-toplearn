d0eb111214d0c8b2c7e56357895390da9075a953
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-12 08:42:15 +0430

    23 express basic routing intro

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ import express from 'express'
 const app = express()
 
 app.use((req, res, next) => {
-    console.log('First middleware')
+    console.log('This middleware runs every time')
     next()
 })
 
-app.use((req, res, next) => {
-    console.log('Second middleware')
-    res.send('<h1>Hello from the second middleware!</h1>')
+app.use('/hello', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.use('/message', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<form action="/message" method="POST"><input type="text" name="message" /><input type="submit" /></form>')
 })
 
 app.listen('3000')
