7c7cee8324ac44bfb839b26bfff878684fa9f6e8
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-12 09:00:19 +0430

    24 body-parser and http methods lesson

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+import fs from 'fs'
+
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+const bodyParserMiddleware = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParserMiddleware) // for using third-party middlewares we must use app.use()
 
 app.use((req, res, next) => {
     console.log('This middleware runs every time')
     next()
 })
 
-app.use('/hello', (req, res) => {
+app.get('/', (req, res, next) => {
+    res.send('<form action="/message" method="POST"><input type="text" name="message" /><input type="submit" /></form>')
+})
+
+app.get('/hello', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.use('/message', (req, res) => {
+app.post('/message', (req, res) => {
     console.log(req.body)
-    res.redirect('/')
+    const message = req.body.message
+    fs.writeFile('./data/message.txt', message, (err) => {
+        if (err) throw err
+        res.redirect('/')
+    })
 })
 
-app.use('/', (req, res, next) => {
-    res.send('<form action="/message" method="POST"><input type="text" name="message" /><input type="submit" /></form>')
-})
 
 app.listen('3000')
 
