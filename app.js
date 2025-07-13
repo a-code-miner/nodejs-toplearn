934518a122d9e9a6df1657286277c422806b1e0b
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-13 09:44:29 +0430

    31 PUG template engine intro lesson

--- app.js
+++ app.js
@@ -5,22 +5,23 @@ import bodyParser from 'body-parser'
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.urlencoded({ extended: false })) // Alternative to bodyParser for parsing URL-encoded data but not recommended
+app.use(express.static('public'))
 
-app.use((req, res, next) => {
-    console.log('First middleware')
-    req.mortaza = { name: 'Mortaza' }
-    next()
-})
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Hello ${req.mortaza.name}</h1>`)
-})
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 app.get('/hello', (req, res) => {
     res.send(`<h1>Hello World</h1>`)
 })
 
+app.get('/', (req, res) => {
+    const arr = [
+        { name: 'Alice', age: 30 },
+        { name: 'Bob', age: 25 },
+        { name: 'Charlie', age: 35 }
+    ]
+    res.render('index', { title: 'Welcome to my app', data: arr })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
