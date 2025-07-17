7a94a0b78a00841fe7a19f7b98272837702c3b1c
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-07-17 12:21:17 +0430

    47 project refactored to async/await

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,17 +2,14 @@ import Todo from '../model/todo.js';
 import { getCompletedTodos, getPendingTodos } from '../utils/todos.js';
 import { completeTodo } from './admin.js';
 
-export function getIndex(req, res) {
-    Todo.count({ where: { completed: true } })
-        .then((completedTodos) => {
-            Todo.findAll()
-                .then((todos) => {
-                    res.render('index', {
-                        pageTitle: 'کارهای روزمره',
-                        todos,
-                        completedTodos,
-                        pendingTodos: todos.length - completedTodos,
-                    });
-                })
-        })
+export async function getIndex(req, res) {
+    const completedTodos = await Todo.count({ where: { completed: true } })
+    const todos = await Todo.findAll()
+
+    res.render('index', {
+        pageTitle: 'کارهای روزمره',
+        todos,
+        completedTodos,
+        pendingTodos: todos.length - completedTodos,
+    });
 }
